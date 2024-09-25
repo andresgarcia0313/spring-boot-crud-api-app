316231b186ec22bcf674693e7f3c4b715a97aaf0
Andrés Eduardo García Márquez <andresgarcia0313@gmail.com>
2024-09-25 14:58:05 -0500

    Refactorizar clase SpringBootCrudAppApplication para cargar variables de entorno al inicio de la aplicación

--- src/main/java/com/company/spring_boot_crud_app/SpringBootCrudAppApplication.java
+++ src/main/java/com/company/spring_boot_crud_app/SpringBootCrudAppApplication.java
@@ -1,13 +1,36 @@
 package com.company.spring_boot_crud_app;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+import org.springframework.lang.NonNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class SpringBootCrudAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootCrudAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        Dotenv.configure().load();
+        SpringApplication.run(SpringBootCrudAppApplication.class, args);
+    }
+}
 
+// Componente para imprimir el mensaje al inicio
+@Component
+class StartupMessageListener implements ApplicationListener<ApplicationReadyEvent> {
+    private static final Logger logger = LoggerFactory.getLogger(StartupMessageListener.class);
+
+    @Value("${server.port}")
+    private int port;
+    @Override
+    public void onApplicationEvent(@NonNull ApplicationReadyEvent event) {
+        logger.info("Aplicación iniciada. Puedes acceder a ella en: http://localhost:{}", port);
+    }
 }
